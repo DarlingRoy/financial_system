dca3229bd65dcf1fe2adb0fda03bb5d0ffc96fc2
DarlingRoy <347180790@qq.com>
2020-06-11 09:38:23 +0800

    update

--- src/main/java/com/example/financial_system/config/handler/CustomizeAuthenticationSuccessHandler.java
+++ src/main/java/com/example/financial_system/config/handler/CustomizeAuthenticationSuccessHandler.java
@@ -3,7 +3,11 @@ package com.example.financial_system.config.handler;
 import com.alibaba.fastjson.JSON;
 import com.example.financial_system.common.entity.JsonResult;
 import com.example.financial_system.common.utils.ResultTool;
+import com.example.financial_system.entity.User;
+import com.example.financial_system.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -11,15 +15,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Component
 public class CustomizeAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
+    @Autowired
+    private UserService userService;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+        org.springframework.security.core.userdetails.User userDetails= (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user=userService.queryByUsername(userDetails.getUsername());
 
         //返回json数据
-        JsonResult result=ResultTool.success();
+        JsonResult result=ResultTool.success(user);
         httpServletResponse.setContentType("text/json;charset=utf-8");
         httpServletResponse.getWriter().write(JSON.toJSONString(result));
     }
