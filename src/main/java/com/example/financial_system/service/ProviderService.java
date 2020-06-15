baaf8ec932ff7d089c552e73e881cb568a1918e0
DarlingRoy <347180790@qq.com>
2020-06-15 18:04:05 +0800

    update

---
+++ src/main/java/com/example/financial_system/service/ProviderService.java
@@ -0,0 +1,70 @@
+package com.example.financial_system.service;
+
+import com.example.financial_system.entity.Provider;
+import java.util.List;
+
+/**
+ * (Provider)表服务接口
+ *
+ * @author laidilin
+ * @since 2020-06-14 23:40:28
+ */
+public interface ProviderService {
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    Provider queryById(Integer id);
+
+    /**
+     * 查询多条数据
+     *
+     * @param offset 查询起始位置
+     * @param limit 查询条数
+     * @return 对象列表
+     */
+    List<Provider> queryAllByLimit(int offset, int limit);
+
+    /**
+     * 查询所有数据
+     *
+     * @return 对象列表
+     */
+    List<Provider> queryAll();
+
+    /**
+     * 新增数据
+     *
+     * @param provider 实例对象
+     * @return 实例对象
+     */
+    Provider insert(Provider provider);
+
+    /**
+     * 修改数据
+     *
+     * @param provider 实例对象
+     * @return 实例对象
+     */
+    Provider update(Provider provider);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 是否成功
+     */
+    boolean deleteById(Integer id);
+
+    /**
+     * 选择性插入数据
+     *
+     * @param provider 实例对象
+     * @return 实例对象
+     */
+    Provider insertSelective(Provider provider);
+
+}
